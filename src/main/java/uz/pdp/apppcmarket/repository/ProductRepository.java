f1640c65e5dd29a326da332bd6d035a076a2bdd5
Khorezmiy1991 <khamza@inbox.ru>
2021-03-31 13:45:56 +0500

    add some changes

--- src/main/java/uz/pdp/apppcmarket/repository/ProductRepository.java
+++ src/main/java/uz/pdp/apppcmarket/repository/ProductRepository.java
@@ -1,12 +1,17 @@
 package uz.pdp.apppcmarket.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import uz.pdp.apppcmarket.entity.Product;
 import uz.pdp.apppcmarket.projection.CustomProduct;
 
 @RepositoryRestResource(path = "product", collectionResourceRel = "productList", excerptProjection = CustomProduct.class)
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-
+    @RestResource(path = "byName")
+    Page<Product> findAllByCity(@Param("product") String city, Pageable pageable);
 }
